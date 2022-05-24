ac0873505c71de4f6c05b6de5f1a8f8efa0df92e
Huy <mitanguy@ucsc.edu>
2022-05-23 18:52:14 -0700

    Updated text

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -2,12 +2,23 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+    preload() {
+
+    }
 
     create() {
         // menu text config
-        let menuTextConfig = {
+        let gameTitleTextConfig = {
             fontFamily: 'Akshar',
             fontSize : '48px',
+            align: 'left',
+            fixedWidth: 0,
+            resolution: 2,
+        }
+
+        let instructionTextConfig = {
+            fontFamily: 'Akshar',
+            fontSize : '24px',
             align: 'left',
             fixedWidth: 0,
             resolution: 2,
@@ -20,7 +31,9 @@ class Menu extends Phaser.Scene {
         this.keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
         // create 'Game Menu' text
-        this.add.text(this.screenCenterX,  this.screenCenterY, "Game Menu", menuTextConfig).setOrigin(0.5);
+        this.add.text(this.screenCenterX,  this.screenCenterY- 50, "Game Menu", gameTitleTextConfig).setOrigin(0.5);
+
+        this.add.text(this.screenCenterX, this.screenCenterY + 50, "PRESS SPACEBAR TO PLAY", instructionTextConfig).setOrigin(0.5);
     }
 
     update() {
