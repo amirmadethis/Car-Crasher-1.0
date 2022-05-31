418289958aafbef7c4d9cf31811efcae90849750
Huy <mitanguy@ucsc.edu>
2022-05-30 20:41:43 -0700

    pause text

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -18,18 +18,22 @@ class Play extends Phaser.Scene {
             resolution: 2,
         }
 
+        // find center of screen
+        this.screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+        this.screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+
+        // game over flag
         this.gameIsOver = false
+
+        // game paused flag
         this.gameIsPaused = false
 
+        // adding pause button
         this.pauseButton = this.add.image(32, 32,'pauseButton').setOrigin(0.5);
         this.pauseButton.setInteractive();
         this.pauseButton.on('pointerdown', () => {
             this.pauseUnpause();
-        });
-
-        // find center of screen
-        this.screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
-        this.screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;        
+        });        
 
         // create background
         this.background = this.add.tileSprite (this.screenCenterX, this.screenCenterY - 1600, 160, 4000, 'background').setOrigin(.5);
@@ -44,6 +48,9 @@ class Play extends Phaser.Scene {
 
         // create player
         this.player = new PlayerCar (this, this.screenCenterX + 25, this.screenCenterY + 200, 'playerCar').setOrigin(0.5);
+
+        this.pauseText = this.add.text(this.screenCenterX, this.screenCenterY, "PAUSED", textConfig).setOrigin(0.5);
+        this.pauseText.alpha = 0;
     }
 
     update() {
@@ -54,7 +61,6 @@ class Play extends Phaser.Scene {
         } else if (keyRIGHT.isDown && (!this.gameIsPaused)) {
             this.player.x += this.player.speed;
         }
-
         if (!this.gameIsOver && !this.gameIsPaused) {
             this.background.tilePositionY -= .3;
         } else {
@@ -66,8 +72,10 @@ class Play extends Phaser.Scene {
     pauseUnpause() {
         if (this.gameIsPaused) {
             this.gameIsPaused = false
+            this.pauseText.alpha = 0
         } else if (!this.gameIsPaused) {
             this.gameIsPaused = true
+            this.pauseText.alpha = 1
         }
     }
 }
